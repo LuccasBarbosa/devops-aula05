f8a86d5006ecdbbd3bd3c81d26123dcbe705d927
ronaldomendes <mendes-ronaldo@live.com>
2020-08-24 20:59:01 -0300

    Adicionando arquitetura.md e ajustes

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,25 +1,17 @@
 def inicializar():
+    tab = []
+    for i in range(3):
+        linha = []
+        for j in range(3):
+            linha.append(".")
+        tab.append(linha)
+    return tab
 
-tab = [ ]
 
-for i in range(3):
+def main():
+    jogo = inicializar()
+    print(jogo)
 
-linha = [ ]
-
-for j in range(3):
-
-linha.append(".")
-
-tab.append(linha)
-
-return tab
-
-def main( ):
-
-jogo = inicializar( )
-
-print (jogo)
 
 if __name__ == "__main__":
-
-main()
+    main()
