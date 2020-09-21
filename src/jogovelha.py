f615e4e57b6088a8dcdcaae7b3d3602de8921129
Luccas Barbosa <luccas@mrdavinci.com.br>
2020-09-21 20:47:21 -0300

    AC2

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,5 +1,4 @@
 def inicializar():
-<<<<<<< HEAD
 	tab = [ ]
 	for i in range(3):
 		linha = [ ]
@@ -14,21 +13,3 @@ def main( ):
 
 if __name__ == "__main__":
 	main()
-=======
-    tab = []
-    for i in range(3):
-        linha = []
-        for j in range(3):
-            linha.append(".")
-        tab.append(linha)
-    return tab
-
-
-def main():
-    jogo = inicializar()
-    print(jogo)
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> f8a86d5006ecdbbd3bd3c81d26123dcbe705d927
