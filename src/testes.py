46ded502bee793a49355d8a18702a2e88651c01a
Luccas Barbosa <luccas@mrdavinci.com.br>
2020-09-21 20:44:52 -0300

    AC2

--- src/testes.py
+++ src/testes.py
@@ -5,7 +5,6 @@ erroInicializar = False
 jogo = jogovelha.inicializar()
 
 if len(jogo) != 3:
-<<<<<<< HEAD
 	erroInicializar = True
 else:
 	for linha in jogo:
@@ -19,18 +18,3 @@ if erroInicializar:
 	sys.exit(1)
 else:
 	sys.exit(0)
-=======
-    erroInicializar = True
-else:
-    for linha in jogo:
-        if len(linha) != 3:
-            erroInicializar = True
-        else:
-            for elemento in linha:
-                if elemento != '.':
-                    erroInicializar = True                  
-if erroInicializar:
-    sys.exit(1)
-else:
-    sys.exit(0)
->>>>>>> f8a86d5006ecdbbd3bd3c81d26123dcbe705d927
